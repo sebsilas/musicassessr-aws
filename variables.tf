6b1916c02f8b7ad0bdb710a1a02d6740d0291cb9
mcetn <56360151+mcetn@users.noreply.github.com>
2021-07-22 12:56:45 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,28 +8,6 @@ variable "project_tags" {
 }
 
 
-variable "aws_ecs_task_definition_params" {
-  description = "Task definition parameters"
-  type        = map(string)
-}
-
-
-variable "container_name" {
-  type = string
-}
-
-
-variable "container_port" {
-  type = number
-}
-
-
-variable "container_definitions_file" {
-  type    = string
-  default = "task.json.tpl"
-}
-
-
 variable "project_name" {
   type = string
 }
