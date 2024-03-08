48afa9afaf6a81124db23b64c5e2251d48bfebe5
Mohamed Etteyeb <mohamed.etteyeb@ticketchainer.com>
2024-03-08 11:22:06 +0100

    Adding support of onset dectection algorithm

--- boilerplate/lambdas/sonic-pyin/app.py
+++ boilerplate/lambdas/sonic-pyin/app.py
@@ -13,18 +13,28 @@ def handler(event, context):
     parsed_s3_source_key = unquoted_s3_source_key.replace(' ', '')
     s3_source_basename = os.path.splitext(os.path.basename(unquoted_s3_source_key))[0]
     s3_destination_filename = s3_source_basename + ".csv"
+
+    s3_response_object = s3_client.get_object(Bucket=s3_source_bucket, Key=unquoted_s3_source_key)
+    s3_source_metadata = s3_response_object['Metadata']
+  
     s3_client.download_file(s3_source_bucket, unquoted_s3_source_key, f'/tmp/{parsed_s3_source_key}')
     command_file_parsed = parsed_s3_source_key.replace('(', '"("').replace(')', '")"')
-    sonic_annotator_cmd = f'./sonic-annotator -d vamp:pyin:pyin:notes -w csv --csv-stdout /tmp/{command_file_parsed}'
+
+    if s3_source_metadata.get("onset","").lower() == "true":
+        print("Metadata contains 'onset' key with value 'true'")
+        sonic_annotator_cmd = f'./sonic-annotator -d vamp:qm-vamp-plugins:qm-onsetdetector:onsets -w csv --csv-stdout /tmp/{command_file_parsed}'
+    else:
+        sonic_annotator_cmd = f'./sonic-annotator -d vamp:pyin:pyin:notes -w csv --csv-stdout /tmp/{command_file_parsed}'
     p1 = subprocess.check_output(sonic_annotator_cmd, shell=True)
 
     sonic_stdout = p1.replace(b'\n,', b'\n')
     sonic_output = sonic_stdout.decode('utf-8').replace(f'"/tmp/{unquoted_s3_source_key}",', '')
-    resp = s3_client.put_object(Body=sonic_output, Bucket=S3_DESTINATION_BUCKET, Key=s3_destination_filename,ACL='public-read')
+    resp = s3_client.put_object(Body=sonic_output, Bucket=S3_DESTINATION_BUCKET, Key=s3_destination_filename, ACL='public-read', Metadata=s3_source_metadata)
 
     return {
-        'statusCode': 200,'headers': {
+        'statusCode': 200,
+        'headers': {
             'Access-Control-Allow-Origin': '*'
         },
-        'body': json.dumps({"result":'Processing complete successfully',"Bucket":S3_DESTINATION_BUCKET,"key":s3_destination_filename})
+        'body': json.dumps({"result": 'Processing complete successfully', "Bucket": S3_DESTINATION_BUCKET, "key": s3_destination_filename})
     }
